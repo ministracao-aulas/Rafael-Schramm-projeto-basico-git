7099aa443cd55cc7f41cd6055c9de1e00c69df12
Tiago França <tiago@tiagofranca.com>
2022-06-09 22:43:58 -0300

    updated index.php
    No ramo master
    Mudanças a serem submetidas:
    	modified:   index.php

--- index.php
+++ index.php
@@ -1,10 +1,82 @@
 <?php
 
-echo "Hello World";
+echo <<<"HTML"
+<!DOCTYPE html>
+<html lang="en">
 
-echo "<hr>";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello World - by TF</title>
+    <style>
+        body {
+            height: 100vh;
+            background: #6e28d9;
+            color: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-echo date('Y-m-d H:i:s');
+        .row.d-flex.justify-content-center {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+        }
 
-echo "<hr>";
-echo "<h5>Algo aqui<h5>";
+        a {
+            color: #fff;
+        }
+
+        a:hover {
+            color: #fff;
+            text-shadow: #000 1px 0px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="row d-flex justify-content-center">
+        <div>
+            <h1>Hello World!</h1>
+            <h2>I am <a href="https://tiagofranca.com" target="_blank">Tiago França</a></h2>
+        </div>
+        <div>
+            <div>
+                <a href="//www.linkedin.com/in/tiago-frança/" target="_blank" title="Linkedin">
+                    Linkedin
+                </a>
+            </div>
+
+            <div>
+                <a href="//t.me/tiagofrancafernandes" title="Telegram">
+                    Telegram
+                </a>
+            </div>
+
+            <div>
+                <a href="//github.com/tiagofrancafernandes" target="_blank" title="GitHub">
+                    GitHub
+                </a>
+            </div>
+
+            <div>
+                <a href="//twitter.com/TiagoFOficial" target="_blank" title="Twitter">
+                    Twitter
+                </a>
+            </div>
+
+            <div>
+                <a href="//www.facebook.com/Tiago.Franca.Fernandes" target="_blank" title="Facebook">
+                    Facebook
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+HTML;
